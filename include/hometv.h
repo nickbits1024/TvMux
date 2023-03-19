74b2f208410bd9b1407e636b67e3fbdb68ef0ea6
Nicholas Palmer <nickbits@gmail.com>
2023-03-19 14:51:35 -0700

    dec max retry

--- include/hometv.h
+++ include/hometv.h
@@ -2,7 +2,7 @@
 #define HOMETV_H
 
 #define ONBOARD_LED_GPIO           2
-#define MAX_COMMAND_RETRY          5
+#define MAX_COMMAND_RETRY          3
 #define MAX_REQUEST_SIZE           1000
 
 extern nvs_handle config_nvs_handle;
